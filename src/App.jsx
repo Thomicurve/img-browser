a71edf573cc894b2fc8e3b5c4b013306b654f4d2
Thomicurve <thomigrax@gmail.com>
2021-04-10 17:19:23 -0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/global.css';
+
+import Header from './components/Header.jsx';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
+  );
+}
+
+export default App;
