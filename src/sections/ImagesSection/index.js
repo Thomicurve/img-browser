6485de3e503860014b1a5987e861d3ae99e22342
Thomicurve <thomigrax@gmail.com>
2021-11-07 23:26:34 -0300

    bugs de las imagenes arreglados / loader al cargar añadido

--- src/sections/ImagesSection/index.js
+++ src/sections/ImagesSection/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Loader from "react-loader-spinner";
 
 import ImagesAPI from '../../services/ApiPixabay';
 import ImageCard from '../../components/ImageCard';
@@ -6,13 +7,14 @@ import ImageCard from '../../components/ImageCard';
 import blob1 from '../../assets/svg/blob-3.svg';
 import blob2 from '../../assets/svg/blob-3.svg';
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './styles.css'
 export default function ImagesSection() {
 
     const [images, setImages] = useState([]);
     const [imageName, setImageName] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     function handleImageName(e) {
         setImageName(e.target.value);
@@ -21,6 +23,7 @@ export default function ImagesSection() {
     async function RandomImages() {
         const results = await ImagesAPI();
         setImages(results);
+        setIsLoading(false);
     }
 
     async function callImagesApi(event) {
@@ -41,16 +44,28 @@ export default function ImagesSection() {
                 <h3 className="browser__title mb-5">Images</h3>
                 <form className="browser__form">
                     <input type="text" onChange={handleImageName} className="" placeholder="Search any image..." />
-                    <button onClick={callImagesApi}><FontAwesomeIcon icon={faSearch}/></button>
+                    <button onClick={callImagesApi}><FontAwesomeIcon icon={faSearch} /></button>
                 </form>
             </article>
             <article className="images-container__images-list">
                 {images.map(img => {
-                    return <ImageCard key={img.id} img={img}/>
+                    return <ImageCard key={img.id} img={img} />
                 })}
             </article>
-            <img className="images-container__blob-1 blobs" src={blob1} alt="blob figure"/>
-            <img className="images-container__blob-2 blobs" src={blob2} alt="blob figure"/>
+            {isLoading &&
+                <div className="loader-container">
+                    <Loader
+                        type="Circles"
+                        color="#F238B4"
+                        height={100}
+                        width={100}
+                    />
+                    <h5>Loading...</h5>
+                </div>
+            }
+
+            <img className="images-container__blob-1 blobs" src={blob1} alt="blob figure" />
+            <img className="images-container__blob-2 blobs" src={blob2} alt="blob figure" />
         </section>
     )
 }
--- src/services/ApiPixabay.js
+++ src/services/ApiPixabay.js
@@ -2,13 +2,16 @@ import axios from 'axios';
 
 const url = `https://pixabay.com/api/?key=14363980-72dcd34efca3ef6e4b8fa0dcc`
 
-const GetImages = async (search) => {
-    let method = '';
-    !search ? method = '' : method = `&q=${search}`;
-
+const GetImages = async (search = 'dogs') => {
     try {
-        const { data } = await axios.get(`${url}${method}`);
-        return data.hits;
+        const { data } = await axios.get(`${url}&q=${search}`);
+
+        if(!data.hits.length) {
+            const { data } = await axios.get(`${url}&q='dogs'`);
+            return data.hits;
+        }else {
+            return data.hits;
+        }
     }catch(err) {
         throw new Error(err)
     }
